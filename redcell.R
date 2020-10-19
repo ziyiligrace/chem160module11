439d9dcb6b5e5992030b1e543a8ee56512cfb25e
ziyiligrace <69704067+ziyiligrace@users.noreply.github.com>
2020-10-19 14:49:45 -0700

    Add files via upload

---
+++ redcell.R
@@ -0,0 +1,10 @@
+redcell<-read.table("redcell.txt", header=T)
+names(redcell)
+is.factor(redcell$ventilation)
+plot(folate~ventilation, data=redcell)
+model<-lm(folate~ventilation, data=redcell)
+summary(model)
+
+
+
+
